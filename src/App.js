d0da87051310f831d298b75013804a0c292a0cb4
KaltygaExceed <kaltyga.exceed@gmail.com>
2021-07-21 13:59:12 +0300

    made new buttons/footer

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import React, {useState} from "react";
 
 
-
 function App() {
     let [todos, setTodo] = useState([])
     let [inputValue, setInputValue] = useState('')
@@ -11,34 +10,53 @@ function App() {
     const onTodo = () => {
         setTodo((todos) => ([
             ...todos,
-            {text: inputValue, id: Date.now()}
+            {text: inputValue, id: Date.now(), isCheck: false}
         ]))
         setInputValue('')
-
     }
-    console.log(inputValue)
+
+    const delTodo = (id) => {
+        setTodo((todos) => ([
+            ...todos.filter(todos => todos.id !== id)
+        ]))
+    }
+
+
     return (
         <div>
-            <header> My ToDo's</header>
+            <header> Your todo list</header>
             <div className='workspace'>
                 <div className='inputElem'>
                     <input id="textInput" placeholder="Enter your task name here" type="text" value={inputValue}
                            onChange={(e) => setInputValue(inputValue = e.target.value)}
+                           onKeyDown={(e) => {
+                               if (e.key === 'Enter') {
+                                   onTodo()
+                               }
+                           }}
                     />
-
-                    <button className='add Button' onClick={onTodo}>
-                        Add
-                    </button>
-
                 </div>
                 <div className='task-container'>
                     {
                         todos.map(todo => (
-                            <div className='tasks' key={todo.id}>{todo.text}
+                            <div className='tasks' key={todo.id}>
+                                <div className='checkbox'><input type="checkbox"/></div>
+                                {todo.text}
                                 <div className='containerWithButtons'><input
-                                    type="button" className='delete Button' value='Delete'/></div>
+                                    type="button" className='delete Button' value='Delete'
+                                    onClick={() => {
+                                        delTodo(todo.id)
+                                    }}/></div>
                             </div>
                         ))
+                    }
+                    {
+                        todos.length > 0 &&
+                        <div className='taskMenu'> {todos.length} tasks left <div className='containerWithMenuButtons'>
+                            <button className='menuButton'>All</button>
+                            <button className='menuButton'>ToDo</button>
+                            <button className='menuButton'>Completed</button>
+                        </div> Clear completed</div>
                     }
 
                 </div>
