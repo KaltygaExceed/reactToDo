c65d8f938683dd80c0bad9108f5cc652cd9b5c09
KaltygaExceed <kaltyga.exceed@gmail.com>
2021-07-29 16:36:35 +0300

    complete logic

--- src/components/TaskContainer/TaskMenu/FilterButtons/FilterButtons.js
+++ src/components/TaskContainer/TaskMenu/FilterButtons/FilterButtons.js
@@ -1,18 +1,28 @@
 import style from './FilterButtons.module.css';
 import React from "react";
-import {useDispatch} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {ViewAll} from "../../../../redux/actions/todosActions";
 import {ViewChecked} from "../../../../redux/actions/todosActions";
 import {ViewUnchecked} from "../../../../redux/actions/todosActions";
 
 function FilterButtons() {
+    const {viewOptions} = useSelector((state) => state.todosReducer)
     const dispatch = useDispatch()
     return (
         <div
             className={style.containerWithMenuButtons}>
-            <button className={style.menuButton} onClick={() => dispatch(ViewAll())}>All</button>
-            <button className={style.menuButton} onClick={() => dispatch(ViewUnchecked())}>ToDo</button>
-            <button className={style.menuButton} onClick={() => dispatch(ViewChecked())}>Completed</button>
+            <button className={
+                (viewOptions === 'all') ? style.menuButton && style.usedButton : style.menuButton
+            } onClick={() => dispatch(ViewAll())}>All
+            </button>
+            <button className={
+                (viewOptions === 'unchecked') ? style.menuButton && style.usedButton : style.menuButton
+            } onClick={() => dispatch(ViewUnchecked())}>ToDo
+            </button>
+            <button className={
+                (viewOptions === 'checked') ? style.menuButton && style.usedButton : style.menuButton
+            } onClick={() => dispatch(ViewChecked())}>Completed
+            </button>
         </div>
     )
 }
